df77c54c8ec9f88523426d34e69d44ba25ada659
U S E R <aisyahrahmadani55@gmail.com>
2025-09-13 11:25:45 +0800

    Menambahkan Fungsi

--- manage_kontak.py
+++ manage_kontak.py
@@ -1,5 +1,29 @@
 "Management Contact Program"
 
+def main():
+    if contact:
+        for num, item in enumerate(contact, start=1):
+            print(f'\n{num}. {item["name"]} : {item["phone"]} : {item["email"]}')
+    else:
+        print('\nNo user added')
+        return 1
+
+def contact():
+    name = input('\nEnter your name: ')
+    phone = input('\nEnter your phone: ')
+    email = input('\nEnter your email: ')
+    new_contact = {'name': name, 'phone': phone, 'email': email}
+    contact.append(new_contact)
+    print('\nUser added successfully!')
+
+def delete():
+    if main() == 1:
+        return
+    else:
+        delete_contact = int(input('\nEnter your choice: '))
+        del contact[delete_contact - 1]
+        print('\nUser deleted successfully!')
+
 contact1 = {'name': "Jorge", 'phone': '680000000', 'email': 'Joe@python.com'}
 contact2 = {'name': "George", 'phone': '68000001','email': 'George@python.com'}
 contact = [contact1, contact2]
@@ -15,28 +39,16 @@ while True:
 
     if choice == '1':
         #List new user
-        if contact:
-            for num, item in enumerate(contact, start=1):
-                print(f'\n{num}. {item["name"]} : {item["phone"]} : {item["email"]}')
-        else:
-            print('\nNo user added')
-            continue
+        main()
+
     elif choice == '2':
         #Add new user and edit user
-        name = input('\nEnter your name: ')
-        phone = input('\nEnter your phone: ')
-        email = input('\nEnter your email: ')
-        new_contact = {'name': name, 'phone': phone, 'email': email}
-        contact.append(new_contact)
-        print('\nUser added successfully!')
+        contact()
+
     elif choice == '3':
         #Delete user
-        for num, item in enumerate(contact, start=1):
-            print(f'\n{num}. {item["name"]} : {item["email"]}')
+        delete()
 
-        delete_contact = int(input('\nEnter your choice: '))
-        del contact[delete_contact-1]
-        print('\nUser deleted successfully!')
     elif choice == '4':
         break
     else:
